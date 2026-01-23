dbd637fa77e5033286c41d4c349432be6bae3e5c
Joseph Abdullaahi <josephabdullaahi@gmail.com>
2026-01-23 14:00:00 +0300

    feat(ui): update button with dark mode styles and shadow support

--- src/Components/ui/Button.jsx
+++ src/Components/ui/Button.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+
 function Button({ children, shadow, onClick, className = "" }) {
   return (
     <button
-      className={`text-base font-light px-4 py-2 rounded-sm flex gap-2 items-center ${className} ${
+      className={`text-sm font-light px-5 py-2 rounded-sm flex gap-2 items-center bg-white dark:bg-blue-900 text-grey-950 dark:text-white transition-colors duration-300 ${className} ${
         shadow ? "shadow-md" : ""
       }`}
       onClick={onClick}
