72c624433a317fee98a0dd00631bf079995c7c79
Joseph Abdullaahi <josephabdullaahi@gmail.com>
2026-01-31 14:30:00 +0300

    feat(layout): add reusable container component

---
+++ src/layout/Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function Container({children, className}) {
+  return (
+    <div className={`container ${className}`} >
+      {children}
+    </div>
+  )
+}
+
+export default Container
